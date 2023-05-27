a46fca9528989c3ffe472b6b1fe06ead8713e08f
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-05-27 06:22:12 +0300

    Update mypush.c

--- mypush.c
+++ mypush.c
@@ -1,18 +1,99 @@
 #include "monty.h"
 
-void _push(stack_t **stack,unsigned int daa) 
+stack_t *add_dnodeint_end(stack_t **head, const int n)
 {
-	stack_t *ptr;
-	int data = atoi(vars.arg);
-	stack_t *new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-		exit(1);
-new_node->n = data;
-new_node->prev = NULL;
-ptr = *stack;
-*stack = new_node;
-new_node->next = ptr;
+	stack_t *temp, *aux;
 
-if (ptr != NULL)
-	ptr->prev = *stack;
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful with the first time*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	temp->next = aux->next;
+	temp->prev = aux;
+	aux->next = temp;
+	return (aux->next);
+}
+
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful with the first time*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	(*head)->prev = temp;
+	temp->next = (*head);
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
+}
+
+/**
+ * _push - pushes an element to the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: no return
+ */
+void _push(stack_t **doubly, unsigned int cline)
+{
+	int n, j;
+
+	if (!vglo.arg)
+	{
+		dprintf(2, "L%u: ", cline);
+		dprintf(2, "usage: push integer\n");
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	for (j = 0; vglo.arg[j] != '\0'; j++)
+	{
+		if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
+		{
+			dprintf(2, "L%u: ", cline);
+			dprintf(2, "usage: push integer\n");
+			free_vglo();
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	n = atoi(vglo.arg);
+
+	if (vglo.lifo == 1)
+		add_dnodeint(doubly, n);
+	else
+		add_dnodeint_end(doubly, n);
 }
