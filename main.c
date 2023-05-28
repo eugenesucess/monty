a37985f7892e442867df4b60d60e01491220509c
Eugene Mutuyimana <eugenemutuyimana15@gmail.com>
2023-05-28 01:55:38 -0700

    fixed

--- main.c
+++ main.c
@@ -3,30 +3,32 @@
 
 void check_input(int argc, char **argv)
 {  
-  if (argc != 2)
-  {
-    fprintf(stderr, "few arguments");
-    exit(EXIT_FAILURE);
-   }
-  if (fd == NULL)
-  {
-    fprintf(stderr, "can't open");
-    exit(EXIT_FAILURE);
-  }
+	FILE *fd;
+	fd = fopen(argv[1], "r");
+	if (argc != 2)
+	{
+		fprintf(stderr, "few arguments");
+		exit(EXIT_FAILURE);
+	}
+	if (fd == NULL)
+	{
+		fprintf(stderr, "can't open");
+		exit(EXIT_FAILURE);
+	}
 
 }
 int main(int argc, char **argv)
 {
-   size_t size;
-  char *buffer;
-  FILE *fd;
-  fd = fopen(argv[1], "r");
-   check_input(argc, argv);
-  while (getline(&buffer, &size, fd) != -1)
-  {
-    printf("%s\n", buffer);
-  }
-  fclose(fd);
-  free(buffer);
-  return 0;
+	size_t size;
+	char *buffer;
+	FILE *fd;
+	fd = fopen(argv[1], "r");
+	check_input(argc, argv);
+	while (getline(&buffer, &size, fd) != -1)
+	{
+	printf("%s\n", buffer);
+	}
+	fclose(fd);
+	free(buffer);
+	return 0;
 }
