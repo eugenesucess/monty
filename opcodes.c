dec9deebb64aa090975db7d92d4a6b9004f71e94
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-05-28 09:45:23 +0300

    opcodes

---
+++ opcodes.c
@@ -0,0 +1,99 @@
+#include "monty.h"
+#include <string.h>
+
+int number;
+
+void (*get_opcode(char *parsed))(stack_t **top, unsigned int)
+{
+	int i = 0;
+	instruction_t opt[] = {
+		{"push", push_stack},
+		{"pall", pall_stack},
+//		{"pint", pint_stack},
+//		{"pop", pop_stack},
+//		{"swap", _swap},
+//		{"nop", _nop},
+		{NULL, NULL}	
+	};
+	
+	while (opt[i].opcode != NULL)
+	{
+		if (strcmp(opt[i].opcode, parsed) == 0)
+		{
+			return (opt[i].f);
+		}
+		else
+		{
+			fprintf(stderr, "L%u: unknown instruction %s\n", line, invInstruction);
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+}
+
+void check_open_file(char **argv)
+{
+	void (*f)(stack_t **, unsigned int);
+	FILE *fd;
+	char *buffer, *parsed, *command;
+	size_t len = 0;
+	ssize_t len_size;
+	unsigned int line_count = 1;
+	stack_t *top = NULL;
+	
+	fd = fopen(argv[1], "r");
+	if (fd = NULL)
+	{
+		fprint(stderr, "can't open file'");
+		exit(EXIT_FAILURE);
+	}
+	
+	while((len_size = getline(&buffer, &len, fd)) != EOF)
+	{
+		parsed = strtok(buffer, " \t\r\n");
+		if (parsed == '\0')
+			continue;
+		strcpy(command, parsed);
+		
+		if (parsed == NULL || parsed[0] == '#')
+			return(1);
+		if (strcmp(parsed, "push") == 0)
+		{
+			parsed = strtok(NULL, "\t\r\n");
+			
+			if (parsed == NULL || is_number(parsed) == -1)
+			{
+				fprintf(stderr, "not an intger");
+				exit(EXIT_FAILURE);
+			}
+			
+			number = atoi(parsed);
+			
+			f = get_opcode(command);
+			f(&top, line_count);
+		}
+		else
+		{
+			f = get_opcode(command);
+			f(&top, line_count);
+		}
+		line_count++;
+	}
+	fclose(fd);
+	free(buffer);
+	free(top);
+}
+int is_number(char *parsed)
+{
+	int i;
+
+	if (parsed == NULL)
+		return (-1);
+
+	for (i = 0; parsed[i] != '\0'; i++)
+	{
+		if (parsed[i] != '-' && isdigit(parsed[i]) == 0)
+			return (-1);
+	}
+	return (1);
+}
