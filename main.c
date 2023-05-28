fb98753fe0661ed96bd9d0ef6e3c61224faaea15
eugenesucess <108212386+eugenesucess@users.noreply.github.com>
2023-05-28 11:51:36 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void read_file(int argc, char **argv)
-{
-   size_t size;
-  char *buffer;
-  FILE *fd;
-  
+void check_input(int argc, char **argv)
+{  
   if (argc != 2)
   {
     fprintf(stderr, "few arguments");
     exit(EXIT_FAILURE);
    }
-  fd = fopen(argv[1], "r");
   if (fd == NULL)
   {
     fprintf(stderr, "can't open");
     exit(EXIT_FAILURE);
   }
-  
+
+}
+int main(int argc, char **argv)
+{
+   size_t size;
+  char *buffer;
+  FILE *fd;
+  fd = fopen(argv[1], "r");
+   check_input(argc, argv);
   while (getline(&buffer, &size, fd) != -1)
   {
     printf("%s\n", buffer);
   }
   fclose(fd);
   free(buffer);
-}
-int main(int argc, char **argv)
-{
-read_file(argc, argv);
   return 0;
 }
